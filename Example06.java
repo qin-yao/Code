81eb2718b03da2e10500627a521c640bc96c2225
qin-yao <55085022+qin-yao@users.noreply.github.com>
2019-09-23 20:25:44 +0800

    final关键字修饰方法
    运行结果表明被final修饰的方法它的子类不能重写该方法

---
+++ Example06.java
@@ -0,0 +1,17 @@
+package lzy;
+//final关键字修饰类
+final class Animal{
+	
+}
+class Dog extends Animal{
+	
+}
+public class Example06 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Dog dog=new Dog();
+
+	}
+
+}
