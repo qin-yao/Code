42013ce31a1cd53c777b0b039762e78bc9d4a0d3
qin-yao <55085022+qin-yao@users.noreply.github.com>
2019-12-03 22:46:11 +0800

    运用多态将计费与积分 移动到Movie类内
    原Rental内getCharge方法与getFrequentRenterPoints方法都是使用了Movie对象的数据，故想到将他俩移动到Movie类中
    这正是体现了面向对象的思想

---
+++ 第六次重构/Rental.java
@@ -0,0 +1,26 @@
+
+public class Rental {
+	private Movie _movie;
+	private int _daysRented;
+	
+	public Rental(Movie movie,int daysRented){
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+	public int getDaysRneted(){
+		return _daysRented;
+		
+	}
+	public Movie getMovie(){
+		return _movie;
+	}
+	
+	double getCharge (){
+		  return _movie.getCharge(_daysRented);  
+	  }
+	
+	int getFrequentRenterPoints(){
+		return _movie.getFrequentRenterPoints(_daysRented);
+	}
+
+}
