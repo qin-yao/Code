4d710f7feabb25880c0071046c37aefafac057c5
qin-yao <55085022+qin-yao@users.noreply.github.com>
2019-12-03 21:54:54 +0800

    讲Customer内的Switch语句 重构为函数

---
+++ 第一次重构/Customer.java
@@ -0,0 +1,72 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+	private String _name;
+	private Vector _rentals = new Vector();
+	
+	public Customer (String name){
+		_name = name;
+	}
+	
+	public void addRental(Rental arg){
+		_rentals.addElement(arg);
+	}
+	public String getName(){
+		return _name;
+	}
+	
+	
+	public String statement(){//Éú³ÉÏêµ¥
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		
+		String result ="Rental Record for"+getName()+"\n";
+		
+		while (rentals.hasMoreElements()){
+			double thisAmount = 0;
+			
+			Rental each = (Rental)rentals.nextElement();
+			
+			thisAmount = amountFor(each);
+			
+			//
+			frequentRenterPoints++;
+			//
+			if((each.getMovie().getPriceCode()==Movie.NEW_RELEASE)
+				&&each.getDaysRneted()>1)
+				frequentRenterPoints++;
+			//
+			result += "\t" +each.getMovie().getTitle()+"\t"+
+				     String.valueOf(thisAmount)+"\n";
+			totalAmount += thisAmount;
+				
+		}
+		result +="Amount owed is" + String.valueOf(totalAmount) +"\n";
+		result +="You earned "+ String.valueOf(frequentRenterPoints )+" frequent renter points";
+		return result;
+   }
+private double amountFor (Rental aRental){
+	  double  result=0;
+	  switch(aRental.getMovie().getPriceCode()){
+		case Movie.REGULAR:
+			result +=2;
+			if(aRental.getDaysRneted()>2)
+				result+= (aRental.getDaysRneted() -2)*1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += aRental.getDaysRneted()*3;
+			break;
+		case Movie.CHILDRENS:
+			result+=1.5;
+			if(aRental.getDaysRneted() >3)
+				result += (aRental.getDaysRneted() -3)*1.5;
+			break;
+		}
+	  return result;
+	  
+  }
+	
+}
+  
